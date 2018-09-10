174467ac88f9f01246b65ec98a65598eda3de8c2
Vger <vcdelariva@gmail.com>
2018-09-10 10:16:40 -0500

    Separate routes into controller file

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+var express = require("express"); // Express Server
+var bodyParser = require("body-parser"); // Post Body Request
+var logger = require("morgan"); // Logger
+// var mongoose = require("mongoose");
+let exphbs = require('express-handlebars'); // Templating Engine
+var db = require("./models"); // Require all models
 
 // Scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+// var axios = require("axios");
+// var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+// var db = require("./models");
 
-var PORT = 3000;
+// Set Default Port for Express and Heroku
+let PORT = process.env.PORT || 3000; 
 
 // Initialize Express
 var app = express();
@@ -26,146 +29,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mediumDB");
-
-// Routes
-
-// A GET route for scraping the Medium/Programming website
-app.get("/scrape", function(req, res) {
-  // First, grab the body of the html with request
-  axios.get("https://medium.com/topic/programming").then(function(response) {
-    // Then, load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    // console.log(`data: ${response.data}`)
-
-    // Now, grab every section with classes l, m & n, and do the following:
-    $("section.l.m.n").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text, href, summary, author & read teime of every article, and save them as properties of the result object
-      result.headline = $(element)
-        .children("div")
-        .children("section")
-        .children("div").first()
-        .children("div").first()
-        .children("div")
-        .children("h3")
-        .children("a")
-        .text();
-      result.link = "https://medium.com" + $(element)
-        .children("div")
-        .children("section")
-        .children("div").first()
-        .children("div").first()
-        .children("div")
-        .children("h3")
-        .children("a")
-        .attr("href");
-      result.summary = $(element)
-        .children("div")
-        .children("section")
-        .children("div").first()
-        .children("div").first()
-        .children("div")
-        .children("div")
-        .children("p")
-        .children("a")
-        .text();
-      result.author = $(element)
-        .children("div")
-        .children("section")
-        .children("div").first()
-        .children("div").last()
-        .children("div")
-        .children("div")
-        .children("a")
-        .children("span")
-        .text();
-      result.readTime = $(element)
-        .children("div")
-        .children("section")
-        .children("div").first()
-        .children("div").last()
-        .children("div")
-        .children("div")
-        .children("div")
-        .children("span")
-        .text();
-      result.dateAdded = Date.now();
-
-      // Do not save to database if empty result
-      if (result.headline !== "") {
-        console.log(`result: ${JSON.stringify(result)}\n`);
-
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
-      }
-    });
-
-    // If successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
-  });
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+// Configure routes
+require("./controllers/mediumController.js")(app);
 
 // Start the server
 app.listen(PORT, function() {
